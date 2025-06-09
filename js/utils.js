c3b975b31f772eeda54e9ae2e23c0c70c28dc290
rezastein <harmonigrupcreator@gmail.com>
2025-06-09 10:07:59 +0700

    09025

--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,20 @@ function hamburgerMenu() {
   const navBar = document.getElementById("nav-navbar");
   navBar.classList.toggle("active");
 }
+function about() {
+  const about_ = document.querySelector(".active-learnmore");
+  const button = document.getElementById("btnLmDes");
+  about_.classList.toggle("show");
+  button.style.display = "none";
+}
+function showPopup(imageSrc) {
+  const popup = document.getElementById("popup");
+  const popupImage = document.getElementById("popupImage");
+  popupImage.src = imageSrc;
+  popup.style.display = "block";
+}
+
+function closePopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
